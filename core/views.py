731a8098bd74c68de19142801148264ea66594f4
Sujal <sujallimbu31@gmail.com>
2026-05-22 15:22:44 +0545

    Added list

--- core/views.py
+++ core/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from .models import Vehicle,VehicleImage
 
 def index(request):
     return render(request, 'core/home.html')
@@ -7,10 +9,34 @@ def profile(request):
     return render(request, 'core/profile.html')
 
 def vehicle_list(request):
-    return render(request , 'core/vehicle_list.html')
+    vehicles = Vehicle.objects.prefetch_related('images').all()
+    return render(request, 'core/vehicle_list.html', {'vehicles': vehicles})
 
+@login_required
 def add_vehicle(request):
-    return render(request , 'core/add_vehicle.html')
+    if request.method == 'POST':
+        name        = request.POST.get('name')
+        vehicle_type = request.POST.get('vehicle_type')
+        year        = request.POST.get('year')
+        description = request.POST.get('description')
+        latitude    = request.POST.get('latitude')
+        longitude   = request.POST.get('longitude')
+
+        vehicle = Vehicle.objects.create(
+            name=name,
+            vehicle_type=vehicle_type,
+            year=year,
+            description=description,
+            latitude=latitude,
+            longitude=longitude,
+        )
+
+        for image in request.FILES.getlist('images'):
+            VehicleImage.objects.create(vehicle=vehicle, image=image)
+
+
+        return redirect('vehicle_list')  
+    return render(request, 'core/add_vehicle.html')
 
 def my_bookings(request):
     return render(request , 'core/my_bookings.html')
