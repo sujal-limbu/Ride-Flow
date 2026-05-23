61e47d593d7929fd5670db77941a639f12370eed
Sujal <sujallimbu31@gmail.com>
2026-05-23 21:20:58 +0545

    Added Filter Form

--- core/views.py
+++ core/views.py
@@ -10,6 +10,23 @@ def profile(request):
 
 def vehicle_list(request):
     vehicles = Vehicle.objects.prefetch_related('images').all()
+    if request.GET.get('name'):
+        vehicles = vehicles.filter(name__icontains=request.GET['name'])
+
+    # if request.GET.get('location'):
+    #     vehicles = vehicles.filter(location__icontains=request.GET['location'])
+
+    if request.GET.get('min_price'):
+        vehicles = vehicles.filter(price_per_day__gte=request.GET['min_price'])
+
+    if request.GET.get('max_price'):
+        vehicles = vehicles.filter(price_per_day__lte=request.GET['max_price'])
+
+    context = {
+        'vehicles': vehicles,
+        # 'locations': Vehicle.objects.exclude(location='').values_list('location', flat=True).distinct(),
+    }
+    # return render(request, 'vehicles/list.html', context)
     return render(request, 'core/vehicle_list.html', {'vehicles': vehicles})
 
 @login_required
